bf9677278cf270da0e8ba2dad129385d72a53b40
Michael Prommer <michael.prommer@outlook.de>
2021-02-12 10:17:38 +0100

    Initial commit

---
+++ Assets/Scripts/Voronoi2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voronoi2D : MonoBehaviour
+{
+    [SerializeField]private Texture2D _texture2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void CreateRandomPointCloud(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 v = new Vector2();
+        }
+    }
+}
