79387d40a5e09dae09ddd66970d6f5a6039ac37a
Michael Prommer <michael.prommer@outlook.de>
2021-02-18 11:41:36 +0100

    modified dcel

--- Assets/Scripts/2D/DCEL2D.cs
+++ Assets/Scripts/2D/DCEL2D.cs
@@ -42,54 +42,157 @@ public class DCEL2D
         }
 
         //4 cases: 2 vertices of triangle already exist(3cases depends on which vertices) or it's the first triangle
+        if (v1Exists && v2Exists)
+        {
+            var e1 = new Edge2D(v1); //A->C
+            var e2 = new Edge2D(v2); //B->C
+            var e3 = new Edge2D(v3); //C->A
+            var e33 = new Edge2D(v3); //C->B
 
-        var e1 = new Edge2D(v1); //A->B
-        var e11 = new Edge2D(v1); //A->C
-        var e2 = new Edge2D(v2); //B->C
-        var e22 = new Edge2D(v2); //B->A
-        var e3 = new Edge2D(v3); //C->A
-        var e33 = new Edge2D(v3); //C->B
+            v3.IncEdge = e3;
 
-        if(!v1Exists) v1.IncEdge = e1;
-        if(!v2Exists) v2.IncEdge = e2;
-        if(!v3Exists) v3.IncEdge = e3;
+            var t = new Triangle(e3);
 
-        var t = new Triangle(e1);
+            e1.left = null;
+            e1.next = e33;
+            e1.prev = v1.IncEdge;
+            e1.twin = e3;
 
-        e1.left = t;
-        e1.next = e2;
-        e1.prev = e3;
-        e1.twin = e22;
+            e2.left = t;
+            e2.next = e3;
+            e2.prev = v1.IncEdge.twin;
+            e2.twin = e33;
 
-        e11.left = null;
-        e11.next = e33;
-        e11.prev = e22;
-        e11.twin = e3;
+            e3.left = t;
+            e3.next = e2.prev;
+            e3.prev = e2;
+            e3.twin = e1;
 
-        e2.left = t;
-        e2.next = e3;
-        e2.prev = e1;
-        e2.twin = e33;
+            e33.left = null;
+            e33.next = e1.prev;
+            e33.prev = e1;
+            e33.twin = e2;
 
-        e22.left = null;
-        e22.next = e11;
-        e22.prev = e33;
-        e22.twin = e1;
+            Triangles.Add(t);
+            Vertices.Add(v3);
+        }
+        else if (v1Exists && v3Exists)
+        {
+            var e1 = new Edge2D(v1); //A->B
+            var e2 = new Edge2D(v2); //B->C
+            var e3 = new Edge2D(v2); //B->A
+            var e33 = new Edge2D(v3); //C->B
 
-        e3.left = t;
-        e3.next = e1;
-        e3.prev = e2;
-        e3.twin = e11;
+            v2.IncEdge = e2;
 
-        e33.left = null;
-        e33.next = e22;
-        e33.prev = e11;
-        e33.twin = e2;
+            var t = new Triangle(e2);
 
-        Triangles.Add(t);
+            e1.left = t;
+            e1.next = e2;
+            e1.prev = v3.IncEdge;
+            e1.twin = e3;
 
-        if(!v1Exists) Vertices.Add(v1);
-        if(!v2Exists) Vertices.Add(v2);
-        if(!v3Exists) Vertices.Add(v3);
+            e2.left = t;
+            e2.next = e1.prev;
+            e2.prev = e1;
+            e2.twin = e33;
+
+            e3.left = null;
+            e3.next = e1.prev.twin;
+            e3.prev = e33;
+            e3.twin = e1;
+
+            e33.left = null;
+            e33.next = e3;
+            e33.prev = e3.next;
+            e33.twin = e2;
+
+            Triangles.Add(t);
+            Vertices.Add(v2);
+        }
+        else if (v2Exists && v3Exists)
+        {
+            var e1 = new Edge2D(v1); //A->B
+            var e2 = new Edge2D(v1); //A->C
+            var e3 = new Edge2D(v2); //B->A
+            var e33 = new Edge2D(v3); //C->A
+
+            v1.IncEdge = e1;
+
+            var t = new Triangle(e1);
+
+            e1.left = t;
+            e1.next = v2.IncEdge;
+            e1.prev = e33;
+            e1.twin = e3;
+
+            e2.left = null;
+            e2.next = v2.IncEdge.twin;
+            e2.prev = e1.next;
+            e2.twin = e33;
+
+            e3.left = null;
+            e3.next = e2;
+            e3.prev = v2.IncEdge.twin;
+            e3.twin = e1;
+
+            e33.left = t;
+            e33.next = e1;
+            e33.prev = e1.next;
+            e33.twin = e2;
+
+            Triangles.Add(t);
+            Vertices.Add(v1);
+        }
+        else
+        {
+            var e1 = new Edge2D(v1); //A->B
+            var e11 = new Edge2D(v1); //A->C
+            var e2 = new Edge2D(v2); //B->C
+            var e22 = new Edge2D(v2); //B->A
+            var e3 = new Edge2D(v3); //C->A
+            var e33 = new Edge2D(v3); //C->B
+
+            v1.IncEdge = e1;
+            v2.IncEdge = e2;
+            v3.IncEdge = e3;
+
+            var t = new Triangle(e1);
+
+            e1.left = t;
+            e1.next = e2;
+            e1.prev = e3;
+            e1.twin = e22;
+
+            e11.left = null;
+            e11.next = e33;
+            e11.prev = e22;
+            e11.twin = e3;
+
+            e2.left = t;
+            e2.next = e3;
+            e2.prev = e1;
+            e2.twin = e33;
+
+            e22.left = null;
+            e22.next = e11;
+            e22.prev = e33;
+            e22.twin = e1;
+
+            e3.left = t;
+            e3.next = e1;
+            e3.prev = e2;
+            e3.twin = e11;
+
+            e33.left = null;
+            e33.next = e22;
+            e33.prev = e11;
+            e33.twin = e2;
+
+            Triangles.Add(t);
+            Vertices.Add(v1);
+            Vertices.Add(v2);
+            Vertices.Add(v3);
+        }
     }
 }
